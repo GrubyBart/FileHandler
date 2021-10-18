a8e8cf1ae74f16e79bc96c022729d0d2ca237bbe
GrubyBart <b.steliga@gmail.com>
2021-10-18 19:58:59 +0200

    kick off new project

---
+++ ConsoleApp6/File.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    public class File
+    {
+        public void Read()
+        {
+            var _streamReader = new StreamReader("C:\\a\\Sample.txt");
+            String line;
+            try
+            {
+                line = _streamReader.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = _streamReader.ReadLine();
+                }
+                _streamReader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void ReadLine(int lineNumber)
+        {
+            var _streamReader = new StreamReader("C:\\a\\Sample.txt");
+            String line;
+            int actualLine = 0;
+            try
+            {
+                line = _streamReader.ReadLine();
+                while (line != null)
+                {
+                    actualLine++;
+                    if (actualLine == lineNumber)
+                    {
+                        Console.WriteLine(line);
+                        break;
+                    }
+                    line = _streamReader.ReadLine();
+                }
+                _streamReader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Write(string text)
+        {
+            var _streamWriter = new StreamWriter("C:\\a\\Sample.txt");
+            try
+            {
+                _streamWriter.WriteLine(text);
+                _streamWriter.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File file = new File();
+
+            
+            
+
+            
+         
+
+        }
+    }
+}
